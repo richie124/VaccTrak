8287f7d4f968cfa9dfc40601b4796e2b36782197
Richard Ching <richardcching@gmail.com>
2021-02-10 12:06:31 -0500

    Working Version with endpoint of "localhost:8080/VaccTrak". VaccCenter model built out. VtController framework built, with one endpoint. VtDao/VtDbDa built enough for app to run. VtService/VtServiceInterface built enough for app to run.

--- vacctrak-backend/src/main/java/com/coolkidz/vacctrak/service/VtService.java
+++ vacctrak-backend/src/main/java/com/coolkidz/vacctrak/service/VtService.java
@@ -1,4 +1,21 @@
 package com.coolkidz.vacctrak.service;
 
-public class VtService {
+import com.coolkidz.vacctrak.data.VtDao;
+import com.coolkidz.vacctrak.models.VaccCenter;
+import com.coolkidz.vacctrak.models.VaccCenter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VtService implements VtServiceInterface {
+
+    private final VtDao vtDao;
+
+    public VtService(VtDao vtDao) {
+        this.vtDao = vtDao;
+    }
+
+    @Override
+    public VaccCenter createVaccCenter() {
+        return null;
+    }
 }
--- vacctrak-backend/src/main/java/com/coolkidz/vacctrak/vaccTrakApp.java
+++ vacctrak-backend/src/main/java/com/coolkidz/vacctrak/vaccTrakApp.java
@@ -3,6 +3,7 @@ package com.coolkidz.vacctrak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class vaccTrakApp {
 
     public static void main(String[] args) {
