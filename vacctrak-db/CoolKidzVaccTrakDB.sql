72f81040ab4a91acec509f849e9c57fec11a157c
FDJay <55559458+FDJay@users.noreply.github.com>
2021-02-11 12:59:04 -0600

    Add files via upload

---
+++ vacctrak-db/CoolKidzVaccTrakDB.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS CoolKidzVaccTrak;
+CREATE DATABASE CoolKidzVaccTrak;
+USE CoolKidzVaccTrak;
+
+create table VaccineSites (
+	VacCenterId int primary key auto_increment,
+    VacCenter varchar(150) not null,
+    Address varchar(100) not null,
+    City varchar(100) not null,
+    StateAbbreviation varchar(2) not null,
+    ZipCode varchar(10) not null,
+    PhoneNumber varchar(15) DEFAULT 'N/A',
+    NumFirstVaccine int8, 
+    NumSecondVaccine int8,
+    Latitude varchar(30),
+    Longitude varchar(30)
+);
+
+create table Users (
+	UserId int primary key auto_increment,
+	FirstName varchar(25) not null, 
+    LastName varchar(25) not null
+);
+
+create table Permissions(
+	PermId int primary key auto_increment,
+    UserId int, 
+    VacCenterId int, 
+    FOREIGN KEY(UserId) REFERENCES Users(UserId),
+    FOREIGN KEY(VacCenterId) REFERENCES VaccineSites(VacCenterId)
+);
