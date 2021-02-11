82f018421efccf034a1f64a1852cd61c863ea6f5
FDJay <55559458+FDJay@users.noreply.github.com>
2021-02-11 08:41:13 -0600

    Add files via upload
    Added latitude and longitude columns and added more data to the CoolKidzVaccTrakData.sql file

---
+++ vacctrak-db/CoolKidzVaccTrakDATA.sql
@@ -0,0 +1,168 @@
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Walgreens", "7150 W. Archer Ave.", "Chicago", "IL", "60638", "(773) 229-1447", 32, 45);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Swedish Hospital", "5145 N California Avenue", "Chicago", "IL", "60625", "(773) 878-8200", 537, 525);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Apicha Primary Care Clinic", "400 Broadway", "Manhattan", "NY", "10013", "(646) 975-6760", 234, 123);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("UI Health", "525 S Racine Ave", "Chicago", "IL", "60607", "(312) 413-5700", 654, 249);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Walmart", "7535 S Ashland Ave", "Chicago", "IL", "60620", "(773) 420-0289", 456, 356);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Stoger Hospital", "1969 Ogden Ave", "Chicago", "IL", "60612", "(312) 864-7250", 893, 465);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Atlanticare Health Services FQHC", "1401 Atlantic Avenue", "Atlantic City", "NJ", "08401", "(609) 572-6040", 1234, 1342);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Riverside Medical Group - Saddle Brook", "299 Market Street", "Saddle Brook", "NJ", "07663", "(201) 368-1717", 3894, 2343);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("CAMcare Health Corporation", "817 Federal Street", "Camden", "NJ", "08102", "(856) 583-2400", 284, 349);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Community Health Center of Asbury Park", "1301 Main St", "Asbury Park", "NJ", "07712", "(732) 774-6333", 483, 524);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("NYC Health Dept. - Downtown Clinic", "125 Worth Street", "Manhattan", "NY", "10013", "(877) 829-4692", 123, 212);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("NYC Health Dept. - Fort Greene Clinic", "295 Flatbush Ave", "Brooklyn", "NY", "11202", "(877) 829-4692", 2343, 1245);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("NYC Vaccine Hub", "485 Clawson Street", "Staten Island", "NY", "10306", "(877) 829-4692", 893, 465);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Adak Community Health Center", "2105 Main St.", "Adak", "AK", "99546", "(907) 592-8383", 1293, 2321);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Bernie's Pharmacy Inc.", "4100 Lake Otis Pkwy", "Anchorage", "IL", "99508", "(907) 562-2138", 432, 465);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("The Children's Clinic", "4001 Dale St.", "Anchorage", "AK", "99508", "(907) 562-2944", 1232, 1234);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Juneau Urgent & Family Care", "8505 Old Dairy Rd.", "Juneau", "AK", "99801", "(907) 790-4111", 643, 576);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Moscone Center South", "747 Howard Street", "San Francisco", "CA", "94103", "(866) 454-8855", 2432, 2476);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, NumFirstVaccine, NumSecondVaccine) 
+VALUES("San Francisco State University", "755 Font Boulevard", "San Francisco", "CA", "94132", 2343, 3523);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Gooding Pharmacy", "414 Main St.", "Gooding", "ID", "83330", "(208) 934-4000", 2343, 3213);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Minidoka Memoria Hospital", "1224 8th St.", "Rupert", "ID", "83350", " (208) 436-0481", 435, 543);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("ACADIANA PEDIATRICS, LLC", "717 Curtis Drive", "Rayne", "LA", "70578", "(337) 334-7554", 654, 765);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Arcadia Family Pharmacy", "1311 Hazel St.", "Arcadia", "LA", "71001", "(318) 263-3948", 334, 634);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Swedish community clinic at Seattle University", "1001 E. James Way", "Seattle", "WA", "98122", 2342, 3322); 
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("1st Care Management", "1120 Hope Rd", "Sandy Springs", "GA", "30350", "(833) 633-4778", 1234, 2131); 
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Appling Healthcare Systems", "163 East Tollison St.", "Baxley", "GA", "31513", 543, 665);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Bell's Family Pharmacy", "4875 Highway 53 E", "Tate", "GA", "30177", "(678) 454-2300", 732, 634);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Berrien County Hospital", "2501 N Patterson St.", "Valdosta", "GA", "31602", "(229) 433-1000", 2323, 3423);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Albany County Public Health Department", "609 S. 2nd Street", "Laramie", "WY", "82070", "(307) 721-2561", 2343, 2342);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Big Horn County Public Health", "417 S. 2nd Street (Sr. Ctr)", "Greybull", "WY", "82426", "(307) 765-2371", 2353, 2345);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Lovell Office", "213 E 3rd St.", "Lovell", "WY", "82431", "(307) 548-6591", 3421, 2212);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Weston County Public Health", "400 Stampede", "Newcastle", "WY", "82701", "(307) 746-4775", 1244, 1212);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Carbon County Public Health", "812 E. Murray St", "Rawlins", "WY", "82301", "307-328-2607", 233, 112);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Sheridan County Public Health", "297 S. Main Street", "Sheridan", "WY", "82801", "(307) 672-5169", 867, 587);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Casper-Natrona County Health Department", "475 South Spruce", "Casper", "WY", "82601", "(07) 235-9340", 231, 223);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Sweetwater County Community Nursing", "333 Broadway, Suite 110", "Rock Springs", "WY", "82901", "(307) 922-5390", 678, 543);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Logan County Resource Center", "432 West 2nd Avenue", "Ranson", "WV", "25438", 1244, 1212);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Lincoln County Health Department", "8008 Court Avenue", "Hamlin", "WV", "25523",  234, 221);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Bountiful woods cross clinic", "596 West 750 South", "Woods Cross", "UT", "84010", "(801) 525-5020", 2743, 2322);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Summit County Health Department", "77 North 50 East", "Coalville", "UT", "84017",  2322, 2323);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, NumFirstVaccine, NumSecondVaccine) 
+VALUES("San Juan Public Health", "735 S 200 W Suite 2", "Blanding", "UT", "84511",  3312, 3212);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Munsey Pharmacy", "106 Administration Road", "Oak Ridge", "TN", "37830",  2234, 2122);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Liberty Pharmacy", "1518 Highway 100", "Centerville", "TN", "37033", "(931) 729-3541", 332, 234);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Genoa Healthcare", "205 W Springdale Ave", "Knoxville", "TN", "37917",  "(865) 312-7278", 4234, 3234);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Centerstone Pharmacy", "511 8th St.", "Clarksville", "TN", "37040", "(931) 920-7252", 2342, 2131);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Access Health – Brookings", "400 22nd Avenue", "Brookings", "SD", "57006", "605-697-9500", 2322, 2551);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Mobridge Regional Hospital", "1401 10th Ave W", "Mobridge", "SD", "57601", "(605) 845-3692", 4323, 2433);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Prisma Health Hillcrest Hospital", "729 SE Main S ", "Simpsonville", "SC", "29681", "833-277-4762", 93484, 33323);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Rock Hill Health Department", "1070 Heckle Blvd ", "Rock Hill", "SC", " 29732", "855-472-3432", 9484, 8380);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Scl Health St. Mary'S Hospital", "2635 N 7Th Street", "Grand Junction", "CO", " 81501", "(970) 298-2273", 7394, 7344);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, PhoneNumber, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Custer County Public Health Agency", "10 Hermit Lane Unit 5 ", "Westcliffe", "CO", " 81252", "(719) 783-3369", 876, 746);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Six Flags America Theme Park", "13710 Central Ave.", "Upper Marlboro", "ND", " 20721", 10887, 10478);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Holy Cross Hospital", "1500 Forest Glen Road", "Silver Spring", "MD", " 20910",  3234, 3434);
+
+INSERT INTO VaccineSites(VacCenter, Address, City, StateAbbreviation, ZipCode, NumFirstVaccine, NumSecondVaccine) 
+VALUES("Luminis Health Doctors Community Medical Center", "8118 Good Luck Road", " Lanham", "MD", "20706", 8766, 7654);
+
+
+
+
+
+
+
